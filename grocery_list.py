9f671a18b383f820b5c838d685dc0499579cbfc2
Phil-Slater <pdsltr@gmail.com>
2021-12-14 15:54:46 -0500

    added functions to delete lists and items from lists

--- grocery_list.py
+++ grocery_list.py
@@ -20,14 +20,18 @@ def display_all_shopping_lists():
     for index in range(0, len(shopping_lists)):
         shopping_list = shopping_lists[index]
         print(f"{index+1}: {shopping_list.name} - {shopping_list.address}")
-        for item in (shopping_list.grocery_items):
-            print(item.name)
+
+        for index1 in range(0, len(shopping_list.grocery_items)):
+            list = shopping_list.grocery_items[index1]
+            print(f"\t{index1+1} - {list.name}")
 
 
 while True:
     print("\nEnter 1 to create a new shopping list")
     print("Enter 2 to add to a list")
     print("Enter 3 to view all shopping lists")
+    print("Enter 4 to delete an item")
+    print("Enter 5 to delete an entire shopping list")
     print("Enter q to quit\n")
 
     choice = input("Enter choice: ")
@@ -47,6 +51,22 @@ while True:
         shopping_list.add_grocery_item(GroceryItem(item))
     elif choice == '3':
         display_all_shopping_lists()
+    elif choice == '4':
+        print("Select a list to delete an item from it: ")
+        display_all_shopping_lists()
+        list_choice = int(input("Enter choice: "))
+        shopping_list = shopping_lists[list_choice - 1]
+        for index1 in range(0, len(shopping_list.grocery_items)):
+            list = shopping_list.grocery_items[index1]
+            print(f"\t{index1+1} - {list.name}")
+        del_choice = int(
+            input("Enter the number associated with the item you'd like to delete: "))
+        shopping_list.grocery_items.pop(del_choice-1)
+    elif choice == '5':
+        print("Which list would you like to delete? ")
+        display_all_shopping_lists()
+        list_choice = int(input("Enter choice: "))
+        shopping_lists.pop(list_choice-1)
     elif choice == 'q':
         break
     else:
